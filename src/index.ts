5f62bb4e7919d4737e5d5ca7bb9ac565d82d5ef5
Tran Hai Trieu <haitrieu2524@gmail.com>
2024-06-05 13:13:52 +0700

    feat: Add CORS to the application

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors, { CorsOptions } from 'cors'
 
 import { ENV_CONFIG } from '~/constants/config'
 import { defaultErrorHandler } from '~/middlewares/error.middlewares'
@@ -22,7 +23,11 @@ initFolders()
 
 const app = express()
 const port = ENV_CONFIG.PORT || 4000
+const corsOptions: CorsOptions = {
+  origin: ENV_CONFIG.CLIENT_URL
+}
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/static', staticRouter)
 app.use('/v1/users', usersRouter)
